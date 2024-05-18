d1d7f62270e0a4764c4350cdff48ca6fcb5cdbcb
Guilherme Lettmann Penha <134228041+guilettmann@users.noreply.github.com>
2024-05-18 18:58:44 -0300

    Update Código AMP-224 PAINEL.cpp

--- Código AMP-224 PAINEL.cpp
+++ Código AMP-224 PAINEL.cpp
@@ -30,10 +30,9 @@ portMUX_TYPE selectorMux = portMUX_INITIALIZER_UNLOCKED;
 
 
 //Declaração das variáveis
-uint16_t power = 0, SelectorPosition = 0, lastPosition = 0, accumulatorTemp, apps = 0, RTD, REGEN = 0, GPS, motorTemp, lowVoltage, StateofCharge, fault_bms, fault_inv, fault_ecu, inversorVoltage, RPM;
+uint16_t power = 0, SelectorPosition, lastPosition = 0, accumulatorTemp, apps = 0, RTD, REGEN = 0, GPS, motorTemp, lowVoltage, StateofCharge, fault_bms, fault_inv, fault_ecu, inversorVoltage, RPM;
 float speed = 0, highVoltage; // revisao das variaveis a serem mostradas
 long double accumulatorCurrent;
-int diferentialValue = 0; //valor do potenciometro do diferencial
 int CurrentForm = 0; // variavel para controle da página atual
 int period = 100; // tempo entre envio de dados para o display
 unsigned long time_now = 0; // variavel para controle de envio de dados para o display
@@ -41,6 +40,7 @@ bool display_lock = false; // false: pode mudar a página, true: nao pode mudar 
 bool botao = false; // rtd eh inicialmente low
 bool ebs = false; // ebs manda booleana
 bool fault_dl = false; // emergencia manda booleana
+
 #define REGEN_PIN GPIO_NUM_15
 portMUX_TYPE REGENbutton = portMUX_INITIALIZER_UNLOCKED;
 
@@ -68,7 +68,7 @@ void setupCan(){
   can_filter_config_t f_config;
   f_config.acceptance_code = (0x7FF << 21);
   f_config.acceptance_mask = ~(0x020 << 21);
-  f_config.single_filter = true;//nao recebe tudo da ECU
+  f_config.single_filter = false;
 
   
   if(can_driver_install(&g_config, &t_config, &f_config) == ESP_OK){
@@ -148,7 +148,7 @@ void Task2code( void * pvParameters )
   while(1)
   { if (digitalRead(REGEN_PIN)==HIGH) {
       botao = HIGH;
-      Serial.println("botao acionado");
+      //Serial.println("botao acionado");
       
     }
   else { botao = LOW;
@@ -181,10 +181,16 @@ void Task2code( void * pvParameters )
           myNex.writeNum("n8.val", fault_bms); //Erro BMS
           myNex.writeNum("n9.val", motorTemp); //temperatura Motores
           myNex.writeNum("n10.val", accumulatorTemp); //Temperatura Acumulador
-          myNex.writeNum("n11.val", fault_inv/100); //Erro Inversor
+          myNex.writeNum("n11.val", fault_inv); //Erro Inversor
           myNex.writeNum("n12.val", accumulatorCurrent); //Corrente Acumulador
           myNex.writeNum("n13.val", StateofCharge); //SOC
           myNex.writeNum("n14.val", fault_ecu); //Erro ECU
+          if (digitalRead(REGEN_PIN)==HIGH) {
+            myNex.writeNum("n13.pco", 2016);
+            myNex.writeNum("t7.pco", 2016);
+          //Se pressionar o botão SoC fica verde
+          }
+          
         break;
         case 2: //Provas Curtas
           vTaskDelay(20 / portTICK_PERIOD_MS);
@@ -196,6 +202,7 @@ void Task2code( void * pvParameters )
           myNex.writeNum("n6.val", speed); //Velocidade
           power = (accumulatorCurrent * inversorVoltage)/1000;
           myNex.writeNum("n7.val", power); //Potência dos motores
+          myNex.writeNum("n8.val", RPM);//rpm motor
         break;
         case 3: //Enduro
           vTaskDelay(20 / portTICK_PERIOD_MS);
@@ -207,6 +214,11 @@ void Task2code( void * pvParameters )
           myNex.writeNum("n6.val", speed); //Velocidade
           myNex.writeNum("n7.val", accumulatorTemp); //Temperatura Acumulador
           myNex.writeNum("n8.val", StateofCharge); //SoC
+          if (digitalRead(REGEN_PIN)==HIGH) {
+            myNex.writeNum("n8.pco", 2016);
+            myNex.writeNum("t2.pco", 2016);
+          //Se pressionar o botão SoC fica verde
+          }
         default:
         break; // tirei o do encoder, add RTC
       }
@@ -227,26 +239,26 @@ void Task3code (void * pvParameters)
     case 0x0B0:
         fault_inv = message1.data[0];
         fault_ecu = (message1.data[2] << (8) | message1.data[1]);
-        Serial.println("JESUS");
+        //Serial.println("JESUS");
       break;
     case 0x0B1:
         motorTemp = (message1.data[3] << 8 | message1.data[2]);
+        RPM = (message1.data[1] << 8 | message1.data[0]);
       break;
     case 0x0B2:
-        inversorVoltage = (message1.data[2] << 8 | message1.data[1]);
+        inversorVoltage = (message1.data[2] << 8 | message1.data[1])/100;
         apps = message1.data[5];
-        speed = (message1.data[4] << 8 | message1.data[3])/1000;
+        speed = (message1.data[4] << 8 | message1.data[3])*(3.6/100);
       break;
     case 0x672:
         fault_bms = message1.data[4];
-        Serial.println("BATEEL GARGALO");
+        //Serial.println("BATEEL GARGALO");
       break;
     case 0x673:
         highVoltage = (message1.data[0] << (8) | message1.data[1]);
       break;
     case 0x674:
         accumulatorCurrent = (message1.data[0] << (8) | message1.data[1]);
-        power = (accumulatorCurrent * highVoltage)/1000;
       break;
     case 0x676:
         StateofCharge = message1.data[0];
@@ -265,6 +277,9 @@ void Task3code (void * pvParameters)
       if (fault_dl = true){
           digitalWrite(FAULT_DL_PIN, HIGH);
         }
+    case 0x1806E5F4:
+      Serial.println(message1.data[4]);
+      //Serial.println("recebendo carregador");
     default:
       break;}
     
@@ -272,7 +287,7 @@ void Task3code (void * pvParameters)
 
     //Envia a mensagem
     can_message_t message2;
-    Serial.println("mandandoCAN");
+    //Serial.println("mandandoCAN");
     message2.identifier = 0x010;         // CAN message identifier
     message2.data_length_code = 6;       // CAN message data length - 6 bytes
     message2.data[0] = (botao | SelectorPosition << 5);
@@ -365,6 +380,23 @@ void setup(){
   attachInterrupt(SELECTOR_PIN_3, selector_change, CHANGE);
   attachInterrupt(SELECTOR_PIN_4, selector_change, CHANGE);
   attachInterrupt(SELECTOR_PIN_5, selector_change, CHANGE);
+
+  //ver em qual página ta 
+  if ((digitalRead(SELECTOR_PIN_1))&&(digitalRead(SELECTOR_PIN_2))&&(digitalRead(SELECTOR_PIN_3))&&(digitalRead(SELECTOR_PIN_4))&&(SelectorPosition == 1)){
+      SelectorPosition = 0;
+      Serial.println(SelectorPosition);}
+  if (digitalRead(SELECTOR_PIN_1) == 0){
+      SelectorPosition = 1;
+      Serial.println(SelectorPosition);}
+  if (digitalRead(SELECTOR_PIN_2) == 0){
+      SelectorPosition = 2;
+      Serial.println(SelectorPosition);}
+  if (digitalRead(SELECTOR_PIN_3) == 0){
+      SelectorPosition = 3;
+      Serial.println(SelectorPosition);}
+  if (digitalRead(SELECTOR_PIN_4) == 0){
+      SelectorPosition = 4;
+      Serial.println(SelectorPosition);}
 
   SetupTasks();
 }
